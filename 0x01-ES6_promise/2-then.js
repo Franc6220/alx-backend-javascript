b96c0c6e0703efa860a65e64fbac083668ab32b5
Franc6220 <samuelwangalibo@yahoo.com>
2024-08-04 19:07:08 +0000

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+// Function that handles a Promise with specific handlers
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
+
