74e1dc70d91e30d80a18f02b5ea6adcf154c10ca
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-16 13:21:53 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+// 0-console.js
+
+/**
+ * Prints the given message to STDOUT.
+ * @param {string} message - The message to print.
+ */
+
+function displayMessage(message){
+	console.log(message);
+}
+
+// Export the function so it can be used in other files.
+module.exports = displayMessage;
