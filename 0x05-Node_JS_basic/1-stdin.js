972384825098bb80148ef4b123ed66b2c69d2471
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-18 17:30:49 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
-// 1-stdin.js
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Hplberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
 	const uname = process.stdin.read();
 
 	if (uname) {
-		process.stdout.write(`Your name is: ${uname.toString().trim()}\n`);
-
-		// Close stdin manually to simulate process end in piped input
-		process.stdin.end();
+		process.stdout.write(`Your name is: ${uname}`);
 	}
 });
 
-process.stdin.on('end', () => {
+process.stdin.on('close', () => {
 	process.stdout.write('This important software is now closing\n');
 });
