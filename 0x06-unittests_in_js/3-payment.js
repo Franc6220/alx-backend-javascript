7f24c66291941997558dde4ea44af2a7eb84e660
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-20 06:38:20 +0300

    Spies

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -4,6 +4,7 @@ const Utils = require('./utils');
 function sendPaymentRequestToApi(totalAmount, totslShipping) {
 	const result = Utils.calculateNumber('SUM', totalAmount, TotalShipping);
 	console.log(`The total is: ${result}`);
+	return result
 }
 
 module.exports = sendPaymentRequestToApi;
