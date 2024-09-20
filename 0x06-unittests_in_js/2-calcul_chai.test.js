3ee723858b509a24c21a5eff8f40f345935c7b68
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-20 05:27:42 +0300

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import calculateNumber from './2-calcul_chai.js';
+
+describe('calculateNumber', () => {
+	describe('SUM', () => {
+		it('should return 6 when inputs are 1.4 and 4.5', () => {
+			expect(calculateNumber('SUM', 1.4, 4.6)).to.equal(6);
+		});
+	});
+
+	describe('SUBTRACT', () => {
+		it('should return -4 when inputs are 1.4 and 4.5',() => {
+			expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+		});
+	});
+
+	describe('DIVIDE', () => {
+		it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+			expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+		});
+
+		it('should return "Error" when dividing by 0', () => {
+			expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+		});
+	});
+});
