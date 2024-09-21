c17d9b93efd808ed61ae4978c69d16241687bd8c
Franc6220 <samuelwangalibo@yahoo.com>
2024-09-21 20:29:41 +0300

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.js
@@ -0,0 +1,8 @@
+// 3-payment.js
+const Utils = require('./utils');
+
+module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
+	const result = utils.calculateNumber('SUM', totalAmount, totalShipping);
+	console.log(`The total is: ${result}`);
+	return result;
+};
