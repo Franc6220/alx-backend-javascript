b88754bc68ebc3b5641f6d0a0715fe15c277c1c7
Franc6220 <samuelwangalibo@yahoo.com>
2024-10-11 12:02:01 +0300

    Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,17 @@
+// Use 'const' for taskFirst function as the variable should not be reassigned
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
+	const task = 'I prefer const when I can.';
+	return task;
 }
 
-export function getLast() {
-  return ' is okay';
-}
-
+// Use 'let' for taskNext function as 'combination' can be reassigned
 export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
+	let combination = 'But sometimes let';
+	combination += getLast();    // Reassigns the value by concatenating
+	return combination;
+}
 
-  return combination;
+// Helper function to return the last part of the sentence
+export function getLast() {
+	return ' is okay';
 }
