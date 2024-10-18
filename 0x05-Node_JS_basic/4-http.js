ed67785a5e8a46fec5fdf9a9a8cc600bf4cf880e
Franc6220 <samuelwangalibo@yahoo.com>
2024-10-18 10:05:40 +0300

    https

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,19 +1,15 @@
 const http = require('http');
 
-// Create the server
+const hostname = '127.0.0.1';
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-	// Set the response code to 200 (OK) 
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-
-	// Send " " in response body
-	res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
 });
 
-// Make the server listen on port 1245
-app.listen(1245, () => {
-	console.log('Server running at http://localhost:1245/');
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
-// Export the server for external use
 module.exports = app;
