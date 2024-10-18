92dcc8649fae5832d3dbdaf60253fb2a58130bfa
Franc6220 <samuelwangalibo@yahoo.com>
2024-10-18 10:59:09 +0300

    tests

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,8 +1,7 @@
-// 3-payment.js
-const Utils = require('./utils');
+const utils = require('./utils');
 
 module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
-	const result = utils.calculateNumber('SUM', totalAmount, totalShipping);
-	console.log(`The total is: ${result}`);
-	return result;
+  const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+  return total;
 };
